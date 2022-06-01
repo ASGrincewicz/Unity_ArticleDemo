2ee372dd917407912253169f0cd9746361e2db30
Aaron S Grincewicz <veganimus@outlook.com>
2022-06-01 17:56:08 -0400

    Player Cam

--- Player.cs
+++ Player.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(Transform))]
+[RequireComponent(typeof(SphereCollider))]
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(Rigidbody))]
 
+    
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _speed;
+    private Transform _transform;
+    private void Awake()
     {
-        
+        var meshFilter = GetComponent<MeshFilter>();
+        var meshRenderer = GetComponent<MeshRenderer>();
+        if (meshFilter)
+            Debug.Log("Mesh Filter Found");
+        if (meshRenderer)
+            Debug.Log("Mesh Renderer Found");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        _transform = transform;
+    }
+
+    private void Update()
+    {
+       Move();
+    }
+
+    private void Move()
+    {
+        var horizontal = Input.GetAxis("Left_Right");
+        var vertical = Input.GetAxis("Forward_Backward");
+        var direction = new Vector3(horizontal, 0.0f, vertical) ;
+
+        _transform.Translate(direction * (_speed * Time.deltaTime));
+
     }
 }
